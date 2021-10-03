6dbc069a89927f75bf7d9e11d00433d6f29a5031
Leired7 <leirediez.7@gmail.com>
2021-10-03 10:12:05 +0200

    Refactor class names to BEM and change to className

--- src/App.js
+++ src/App.js
@@ -6,36 +6,36 @@ import iconWeight from './assets/icons/icon-weight.svg';
 function App() {
   return (
     <div className="App">
-      <article class="poke-card">
-        <header class="card-header">
-          <h2 class="card-title">Bulbasaur</h2>
-          <p class="card-id">#001</p>
+      <article class="card">
+        <header class="card__header">
+          <h2 class="card__title color-white">Bulbasaur</h2>
+          <p class="card__id color-white">#001</p>
         </header>
-        <img src={Bulbasaur} alt="" class="card-image" />
-        <div class="card-description">
-          <ul class="card-tag-list">
-            <li class="card-tag tag bg-grass">Grass</li>
-            <li class="card-tag tag bg-poison">Poison</li>
+        <img src={Bulbasaur} alt="" class="card__image" />
+        <div class="card__description">
+          <ul class="card__list">
+            <li class="card-tag tag background-color-grass">Grass</li>
+            <li class="card-tag tag background-color-poison">Poison</li>
           </ul>
-          <div class="card-about">
-            <h3 class="card-about-title c-grass">About</h3>
-            <ul class="card-icons">
-              <li class="icon-wrapper">
-                <p class="icon-text">
+          <div class="card__about">
+            <h3 class="card__about-title color-grass">About</h3>
+            <ul class="card__icons">
+              <li class="data__wrapper">
+                <p class="data__text">
                   <img src={iconWeight} alt="Icono de una regla" />
-                  <span class="type-number">6,9 kg</span>
+                  <span class="data__number">6,9 kg</span>
                 </p>
-                <span class="type">Weight</span>
+                <span class="data_type">Weight</span>
               </li>
-              <li class="icon-wrapper">
-                <p class="icon-text">
+              <li class="data__wrapper">
+                <p class="data__text">
                   <img src={iconHeight} alt="Icono de una regla" />
-                  <span class="type-number">0,7 m</span>
+                  <span class="data__number">0,7 m</span>
                 </p>
-                <span class="type">Height</span>
+                <span class="data__type">Height</span>
               </li>
             </ul>
-            <p class="pokemon-description truncate-overflow">
+            <p class="about__description truncate-overflow">
               There is a plant seed on its back right from the day this Pokémon
               is born. The seed slowly grows larger.
             </p>
