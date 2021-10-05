0cca35343e0d3a0f334c5b0a39fb5b74bd7837e8
Leired7 <leirediez.7@gmail.com>
2021-10-05 19:47:10 +0200

    Add dynamic card info and custom styles

--- src/App.js
+++ src/App.js
@@ -84,42 +84,58 @@ function App() {
         </form>
       </div>
       <section className="section__container">
-        <article className="card">
-          <header className="card__header">
-            <h2 className="card__title color-white">Bulbasaur</h2>
-            <p className="card__id color-white">#001</p>
-          </header>
-          <img src={Bulbasaur} alt="" className="card__image" />
-          <div className="card__description">
-            <ul className="card__list">
-              <li className="card-tag tag background-color-grass">Grass</li>
-              <li className="card-tag tag background-color-poison">Poison</li>
-            </ul>
-            <div className="card__about">
-              <h3 className="card__about-title color-grass">About</h3>
-              <ul className="card__icons">
-                <li className="data__wrapper">
-                  <p className="data__text">
-                    <img src={iconWeight} alt="Icono de una regla" />
-                    <span className="data__number">6,9 kg</span>
+        {pokeTest.map((pokeitem) => {
+          return (
+            <article
+              className={`card background-color-${pokeitem.types[0].type.name}`}
+            >
+              <header className="card__header">
+                <h2 className="card__title color-white">{pokeitem.name}</h2>
+                <p className="card__id color-white">#{pokeitem.id}</p>
+              </header>
+              <img src={Bulbasaur} alt="" className="card__image" />
+              <div className="card__description">
+                <ul className="card__list">
+                  {pokeitem.types.map((pokeTag) => {
+                    return (
+                      <li
+                        className={`card-tag tag background-color-${pokeTag.type.name}`}
+                      >
+                        {pokeTag.type.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <div className="card__about">
+                  <h3
+                    className={`card__about-title color-${pokeitem.types[0].type.name}`}
+                  >
+                    About
+                  </h3>
+                  <ul className="card__icons">
+                    <li className="data__wrapper">
+                      <p className="data__text">
+                        <img src={iconWeight} alt="Icono de una regla" />
+                        <span className="data__number">{pokeitem.weight}</span>
+                      </p>
+                      <span className="data__type">Weight</span>
+                    </li>
+                    <li className="data__wrapper">
+                      <p className="data__text">
+                        <img src={iconHeight} alt="Icono de una regla" />
+                        <span className="data__number">{pokeitem.height}</span>
+                      </p>
+                      <span className="data__type">Height</span>
+                    </li>
+                  </ul>
+                  <p className="about__description truncate-overflow">
+                    {pokeitem.description}
                   </p>
-                  <span className="data__type">Weight</span>
-                </li>
-                <li className="data__wrapper">
-                  <p className="data__text">
-                    <img src={iconHeight} alt="Icono de una regla" />
-                    <span className="data__number">0,7 m</span>
-                  </p>
-                  <span className="data__type">Height</span>
-                </li>
-              </ul>
-              <p className="about__description truncate-overflow">
-                There is a plant seed on its back right from the day this
-                Pokémon is born. The seed slowly grows larger.
-              </p>
-            </div>
-          </div>
-        </article>
+                </div>
+              </div>
+            </article>
+          );
+        })}
       </section>
     </div>
   );
