886b8bf47502418d21a65700d0cb2cf9430b9e67
Leired7 <leirediez.7@gmail.com>
2021-10-10 13:09:09 +0200

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Header } from './components/Header';
 import { SearchBar } from './components/SearchBar';
 import { PokeCard } from './components/Pokecard';
+import { Footer } from './components/Footer';
 
 import iconHeight from './assets/icons/icon-height.svg';
 import iconWeight from './assets/icons/icon-weight.svg';
@@ -54,20 +55,7 @@ function App() {
           iconWeight={iconWeight}
         />
       </section>
-      <footer>
-        <ul className="footer__container">
-          <li className="footer__link">
-            <a href="https://github.com/Leired7/pokedex">
-              <img src={iconGithub} alt="Logo Github" />
-            </a>
-          </li>
-          <li className="footer__link">
-            <a href="https://pokeapi.co/">
-              <img src={logoPokeapi} alt="Logo Poke API" />
-            </a>
-          </li>
-        </ul>
-      </footer>
+      <Footer iconGithub={iconGithub} logoPokeapi={logoPokeapi} />
     </main>
   );
 }
