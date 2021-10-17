b6ae1a5d774624c6e00338eee2ddaffcfa37e989
Leired7 <leirediez.7@gmail.com>
2021-10-17 18:40:08 +0200

    Add loading state in useEffect when is calling th API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
+
 import './App.css';
+
+import { Loading } from './components/Loading';
 import { Header } from './components/Header';
 import { SearchBar } from './components/SearchBar';
 import { PokeCard } from './components/Pokecard';
@@ -13,6 +16,7 @@ import logoPokeapi from './assets/icons/logo-pokeapi.svg';
 function App() {
   const [pokeCards, setPokeCards] = useState([]);
   const [filter, setFilter] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const filteredPokemon =
     pokeCards.length > 0
@@ -22,22 +26,28 @@ function App() {
       : [];
 
   useEffect(() => {
+    setLoading(true);
+
     async function getPokecards() {
-      const pokeApiInfo = await fetch('https://pokeapi.co/api/v2/pokemon').then(
-        (res) => res.json()
-      );
+      try {
+        const pokeApiInfo = await fetch(
+          'https://pokeapi.co/api/v2/pokemon'
+        ).then((res) => res.json());
 
-      const pokemonUrls = pokeApiInfo.results.map((item) => item.url);
+        const pokemonUrls = pokeApiInfo.results.map((item) => item.url);
 
-      const pokeCardFetched = await Promise.all(
-        pokemonUrls.map(async (url) => {
-          const resp = await fetch(url);
-          return resp.json();
-        })
-      );
+        const pokeCardFetched = await Promise.all(
+          pokemonUrls.map(async (url) => {
+            const resp = await fetch(url);
+            return resp.json();
+          })
+        );
 
-      console.log(pokeCardFetched[3]);
-      setPokeCards(pokeCardFetched);
+        setPokeCards(pokeCardFetched);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
     }
 
     getPokecards();
@@ -48,12 +58,18 @@ function App() {
       <Header />
       <SearchBar filter={filter} onChangeSetFilter={setFilter} />
 
-      <section className="section__container">
-        <PokeCard
-          filteredPokemon={filteredPokemon}
-          iconHeight={iconHeight}
-          iconWeight={iconWeight}
-        />
+      <section
+        className={`section__container ${loading ? 'loading-center' : ''}`}
+      >
+        {loading ? (
+          <Loading />
+        ) : (
+          <PokeCard
+            filteredPokemon={filteredPokemon}
+            iconHeight={iconHeight}
+            iconWeight={iconWeight}
+          />
+        )}
       </section>
       <Footer iconGithub={iconGithub} logoPokeapi={logoPokeapi} />
     </main>
